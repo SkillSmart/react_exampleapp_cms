c17d5061a7685a8775242abd36635d3d685a9708
Frank Fichtenmueller <frank.fichtenmueller@outlook.com>
2017-12-27 12:07:55 +0530

    intialized server setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const Hapi = require('hapi')
+
+
+const startUp = async () => {
+
+    const server = new Hapi.Server({
+        host: 'localhost',
+        port: 3000
+    });
+
+    // Register Plugins
+
+    // Start the server
+    await server.start()
+    console.log(`Server successfully started at ${server.info.uri}`);
+}
+
+startUp();
